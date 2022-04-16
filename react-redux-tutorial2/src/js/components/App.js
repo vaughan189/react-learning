32edb7c5854ad0b072623019fa8e03009b611dc2
Vaughan Po <vaughan188@gmail.com>
2022-04-16 16:57:28 +0530

    react redux tutorials 2

---
+++ react-redux-tutorial2/src/js/components/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Form from "./Form";
+import List from "./List";
+import Post from "./Posts";
+
+const App = () => (
+  <>
+    <div>
+      <h2>Articles</h2>
+      <List />
+    </div>
+    <div>
+      <h2>Add a new article</h2>
+      <Form />
+    </div>
+    <div>
+      <h2>API posts</h2>
+      <Post />
+    </div>
+  </>
+);
+
+export default App;
