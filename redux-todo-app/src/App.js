3cff1c0e3115aa6701823af83844311cda90d035
Vaughan Po <vaughan188@gmail.com>
2022-04-16 11:24:39 +0530

    redux todo app baseline setup

---
+++ redux-todo-app/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Todos from "./components/Todos";
+import DisplayTodos from "./components/DisplayTodos";
+
+function App() {
+  return (
+    <>
+      <Todos />
+      <DisplayTodos />
+    </>
+  );
+}
+
+export default App;
