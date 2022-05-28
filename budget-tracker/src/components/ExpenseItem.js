8b6e8a14d8e030c503f60eedbfa85ea7d7c5a9f8
Vaughan Po <vaughan188@gmail.com>
2022-05-28 12:57:47 +0530

    budget tracker app added

---
+++ budget-tracker/src/components/ExpenseItem.js
@@ -0,0 +1,33 @@
+import React, { useContext } from "react";
+import { TiDelete } from "react-icons/ti";
+import { AppContext } from "../context/AppContext";
+
+const ExpenseItem = (props) => {
+  const { dispatch } = useContext(AppContext);
+
+  const handleDeleteExpense = () => {
+    dispatch({
+      type: "DELETE_EXPENSE",
+      payload: props.id,
+    });
+  };
+
+  return (
+    <li className="list-group-item d-flex justify-content-between align-items-center">
+      {props.name}
+      <div>
+        <span className="badge badge-primary badge-pill mr-3">
+          ₹{props.cost}
+        </span>
+        {/* TODO: ADD pointer curser CSS */}
+        <TiDelete
+          size="1.5em"
+          style={{ cursor: "pointer" }}
+          onClick={handleDeleteExpense}
+        ></TiDelete>
+      </div>
+    </li>
+  );
+};
+
+export default ExpenseItem;
